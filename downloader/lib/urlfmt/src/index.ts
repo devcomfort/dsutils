a8eb108a4ec2e37f3eefee7fae29d24a737684e0
devcomfort <kimdonghyun026@gmail.com>
2023-03-20 00:33:27 +0900

    feat : 다운로드 핸들러, URL 포메터 추가

---
+++ downloader/lib/urlfmt/src/index.ts
@@ -0,0 +1,14 @@
+import isURL from "validator/es/lib/isURL";
+
+const formatURL = (url: string) => {
+  if (!isURL(url)) throw new Error(`잘못된 URL 형식입니다`);
+
+  const URLObject = new URL(url);
+  URLObject.host = "dcms.dongseo.ac.kr";
+  URLObject.port = "";
+
+  return url.toString();
+};
+
+export default formatURL;
+export { formatURL };
