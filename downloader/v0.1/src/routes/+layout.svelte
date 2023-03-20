0f1f0313da2a4943cf73a700fab6bb7f6d1d3f48
devcomfort <kimdonghyun026@gmail.com>
2023-03-20 11:46:02 +0900

    feat : 기본 레이아웃 추가

---
+++ downloader/v0.1/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { SvelteUIProvider, globalCss } from "@svelteuidev/core";
+  import { onMount } from "svelte";
+
+  // 컬러 팔레트 참조 : https://www.pinterest.co.kr/pin/196610339977626951/
+  const globalStyles = globalCss({
+    body: {
+      margin: 0,
+      width: "100vw",
+      height: "100vh",
+      color: "#ffffff",
+      backgroundColor: "#191e29",
+    },
+  });
+
+  onMount(() => {
+    globalStyles();
+  });
+</script>
+
+<SvelteUIProvider withNormalizeCSS withGlobalStyles>
+  <slot />
+</SvelteUIProvider>
