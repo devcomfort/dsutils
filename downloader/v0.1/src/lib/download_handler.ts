8e02f28b2de03a400f75769f35fc9f18a3bb5c9e
devcomfort <kimdonghyun026@gmail.com>
2023-03-20 22:54:28 +0900

    feathering : 파일 이름에 대한 기능을 추가하는 중

--- downloader/v0.1/src/lib/download_handler.ts
+++ downloader/v0.1/src/lib/download_handler.ts
@@ -1,5 +1,6 @@
 import byteSize from "byte-size";
 import download from "downloadjs";
+import { fileTypeFromFile } from "file-type";
 import isURL from "./isurl";
 
 interface InstanceOptions {
@@ -34,13 +35,28 @@ const createFetchInstance = (opts: InstanceOptions) => {
     ) as Promise<`${number}`>;
   const sizeAsString = () => size().then((size) => byteSize(parseInt(size)));
 
+  const getFilenameFrom = async (url: string, name?: string) => {
+    const [urlExts, nameExts, actualMimeType] = await Promise.all([
+      fileTypeFromFile(url),
+      fileTypeFromFile(name || ""),
+      mimeType(),
+    ]);
+
+    // TODO : 로직 마저 작성하기
+    // 1. name이 입력되지 않음.
+    //    -> URL의 Mime타입과 이름을 기반으로 파일 이름 생성
+    // 2. name이 입력되었으나 파일 확장자를 특정할 수 없음.
+    //    -> 이름이 명확하다면 파일 확장자만 수정
+    // 3. name이 입력되었으나 파일 확장자가 원본 파일이랑 다름.
+    //    -> 파일 확장자만 수정
+  };
+
   const get = () => fetch(opts.url, { method: "GET" });
   const blob = () => get().then((response) => response.blob());
   const _download = (name?: string) =>
-    blob().then((blob) => {
-      const _name = name || new URL(opts.url).pathname.split("/").at(-1);
-      return download(blob, _name);
-    });
+    Promise.all([blob(), getFilenameFrom(opts.url, name)]).then(
+      ([blob, name]) => download(blob, name)
+    );
 
   const _downloadWithProgress = async (
     name: string = "",
