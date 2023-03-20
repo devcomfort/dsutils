074463ebc5600299575aa43d2cdfb9e146dc2080
devcomfort <kimdonghyun026@gmail.com>
2023-03-20 11:44:20 +0900

    fix : 오류를 반환하도록 변경

--- downloader/lib/urlfmt/src/index.ts
+++ downloader/lib/urlfmt/src/index.ts
@@ -1,7 +1,7 @@
 import isURL from "validator/es/lib/isURL";
 
 const formatURL = (url: string) => {
-  if (!isURL(url)) throw new Error(`잘못된 URL 형식입니다`);
+  if (!isURL(url)) return new Error(`잘못된 URL 형식입니다`);
 
   const URLObject = new URL(url);
   URLObject.host = "dcms.dongseo.ac.kr";
