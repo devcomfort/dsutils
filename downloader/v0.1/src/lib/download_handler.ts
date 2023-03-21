2db389cf98d41b290a9146ad6fe39fee1cf7c08c
devcomfort <kimdonghyun026@gmail.com>
2023-03-21 20:40:27 +0900

    feat : 파일 이름 추출 함수 추가, basename 추출 함수 변경

--- downloader/v0.1/src/lib/download_handler.ts
+++ downloader/v0.1/src/lib/download_handler.ts
@@ -1,12 +1,41 @@
 import byteSize from "byte-size";
 import download from "downloadjs";
 import { fileTypeFromBuffer } from "file-type";
-import parseFilepath from "parse-filepath";
 import isURL from "./isurl";
 
 interface InstanceOptions {
   url: string;
 }
+
+/**
+ * 파일 이름 추출 함수
+ *
+ * URL과 별도로 입력된 이름을 입력받아, 이름이 없다면 URL에서, 이름이 있다면 이름에서 파일 이름을 추출해주는 함수 (-> basename)
+ */
+const getFilename = (url: string, name: string): string => {
+  const extract = (filename: string) => filename.split("/").at(-1);
+  const [urlName, nameName] = [url, name].map(extract);
+  return nameName ? nameName : urlName || "";
+};
+
+const getBasename = async (
+  url: string,
+  name: string,
+  file: Blob | ArrayBuffer
+) => {
+  if (file instanceof Blob) file = await file.arrayBuffer();
+  // 데이터 구조를 분석하여 확장자 추론
+  const inferencedMimeType = await fileTypeFromBuffer(file);
+
+  // 1. 이름이 있을 땐 이름에서 이름을 가져오고
+  // 2. 이름이 없을 땐 URL에서 이름을 추출해오고
+  const filename = getFilename(url, name);
+
+  // 3. 확장자는 Blob 객체를 통해 추정하여 입력하되, 없다면 파일 이름에서 추출 시도, 파일 이름에서도 확장자를 추론할 수 없다면 파일 이름만 달아서 내보내기
+  const ext = inferencedMimeType?.ext || "";
+
+  return ext.length > 0 ? `${filename}.${ext}` : `${filename}`;
+};
 
 const createFetchInstance = (opts: InstanceOptions) => {
   if (!(opts instanceof Object && isURL(opts.url))) return;
@@ -36,34 +65,16 @@ const createFetchInstance = (opts: InstanceOptions) => {
     ) as Promise<`${number}`>;
   const sizeAsString = () => size().then((size) => byteSize(parseInt(size)));
 
-  const getFilename = async (
-    url: string,
-    name: string,
-    file: Blob | ArrayBuffer
-  ) => {
-    if (file instanceof Blob) file = await file.arrayBuffer();
-    const [urlMeta, nameMeta] = [url, name].map((obj) => parseFilepath(obj));
-    const inferencedMimeType = await fileTypeFromBuffer(file);
-
-    // 1. 이름이 있을 땐 이름에서 이름을 가져오고
-    // 2. 이름이 없을 땐 URL에서 이름을 추출해오고
-    const filename = nameMeta.name.length > 0 ? nameMeta.name : urlMeta.name;
-
-    // 3. 확장자는 Blob 객체를 통해 추정하여 입력하되, 없다면 파일 이름에서 추출 시도, 파일 이름에서도 확장자를 추론할 수 없다면 파일 이름만 달아서 내보내기
-    const ext = inferencedMimeType?.ext || "";
-
-    return ext.length > 0 ? `${filename}.${ext}` : `${filename}`;
-  };
-
   const get = () => fetch(opts.url, { method: "GET" });
   const blob = () => get().then((response) => response.blob());
+
   const _download = async (name?: string) => {
     const _blob = await blob();
-    const filename = await getFilename(opts.url, name || "", _blob);
+    const filename = await getBasename(opts.url, name || "", _blob);
     return download(_blob, filename);
   };
 
-  // TODO : fileTypeFromBuffer, getFilename 호출에 의해 각각 fileTypeFromBuffer가 2번 호출됩니다.
+  // TODO : fileTypeFromBuffer, getBasename 호출에 의해 각각 fileTypeFromBuffer가 2번 호출됩니다.
   // 중복 호출이므로 최적화를 권장합니다.
   const _downloadWithProgress = async (
     name: string = "",
@@ -113,7 +124,7 @@ const createFetchInstance = (opts: InstanceOptions) => {
         return blob;
       })
       .then(async (blob) => {
-        return download(blob, await getFilename(opts.url, name || "", blob));
+        return download(blob, await getBasename(opts.url, name || "", blob));
       });
   };
 
@@ -129,5 +140,5 @@ const createFetchInstance = (opts: InstanceOptions) => {
   };
 };
 
-export { createFetchInstance };
+export { createFetchInstance, getFilename };
 export default createFetchInstance;
