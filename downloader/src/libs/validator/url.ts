4144fbd3f465b783a3062e3b3ff56100cf088314
devcomfort <kimdonghyun026@gmail.com>
2024-06-21 11:10:55 +0000

    refactor : validate -> is 메소드 사용하도록 수정

--- downloader/src/libs/validator/url.ts
+++ downloader/src/libs/validator/url.ts
@@ -2,5 +2,4 @@ import typia, { tags } from "typia";
 
 export type TURL = string & tags.Format<"url">;
 
-export const isURL = (url: string): url is TURL =>
-  typia.validate<TURL>(url).success;
+export const isURL = (url: string): url is TURL => typia.is<TURL>(url);
