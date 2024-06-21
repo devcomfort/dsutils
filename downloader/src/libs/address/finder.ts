e69bf961de35ae680771579d0dd25bd419d3e6bf
devcomfort <kimdonghyun026@gmail.com>
2024-06-21 16:38:16 +0000

    fix, comment : Loader 클래스 호출 방식 변경, 주석 설명 개선

--- downloader/src/libs/address/finder.ts
+++ downloader/src/libs/address/finder.ts
@@ -1,13 +1,14 @@
-import { Loader } from "./loader";
+import Loader from "./loader";
 import { IMirrorHost } from "./schema";
-
 import { isURL } from "../validator/url";
 
 // NOTE: 검색 결과로 나온 IMirrorHost가 2개 이상인 경우, 사용자 경험을 위해 학교명을 표시할 수 있도록 그대로 배열 형태로 반환합니다
 // URL만 추출하여 1-D 배열 형태로 반환할까 하다가, 정보량이 높을 수록 추후에 할 수 있는 것이 많아지기에 이렇게 결정했습니다
 
 /**
- * {@link Loader} 클래스에서 얻은 데이터를 조회하는 클래스입니다
+ * {@link Loader} 클래스가 로드한 데이터를 조회하는 기능을 제공함
+ *
+ * @see {@link Loader} 미러 호스트 레지스트리 정보를 로드함
  */
 class Finder {
   private loader: Loader;
