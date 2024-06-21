e9739c8cc6d962fd77794bb1805f8666445e4128
devcomfort <kimdonghyun026@gmail.com>
2024-06-21 09:38:04 +0000

    test : isURL 함수 테스트 추가

---
+++ downloader/src/libs/validator/url.test.ts
@@ -0,0 +1,31 @@
+import { it, expect, describe } from "vitest";
+import { isURL } from "./url";
+import { randomString } from "remeda";
+
+describe("isURL 메소드 테스트", () => {
+  it("유효하지 않은 자료형 입력 (boolean)", () => {
+    // @ts-ignore
+    expect(isURL(true)).toBe(false);
+    // @ts-ignore
+    expect(isURL(false)).toBe(false);
+  });
+
+  it("유효하지 않은 자료형 입력 (number)", () => {
+    // @ts-ignore
+    expect(isURL(Math.random())).toBe(false);
+  });
+
+  it("유효하지 않은 문자열 입력 (random string)", () => {
+    // @ts-ignore
+    expect(isURL(randomString())).toBe(false);
+  });
+
+  it("유효하지 않은 URL 입력", () => {
+    expect(isURL("www.google.com")).toBe(false);
+  });
+
+  it("유효한 URL 입력", () => {
+    expect(isURL("https:www.google.com")).toBe(true);
+    expect(isURL("https://www.google.com")).toBe(true);
+  });
+});
