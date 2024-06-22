f6e9bddc00d0f213177266621447ecc5907911f0
devcomfort <kimdonghyun026@gmail.com>
2024-06-22 14:35:02 +0000

    chore(deps) : tailwindcss, solidUI 설정

--- downloader/vite.config.ts
+++ downloader/vite.config.ts
@@ -1,7 +1,14 @@
+import path from "path";
+
 import UnpluginTypia from "@ryoppippi/unplugin-typia/vite";
 import solid from "vite-plugin-solid";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [solid(), UnpluginTypia()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
 });
