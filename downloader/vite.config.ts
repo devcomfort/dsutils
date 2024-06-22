1ad7c9796856f1673a2108693d453674be9d3d33
devcomfort <kimdonghyun026@gmail.com>
2024-06-22 13:14:53 +0000

    chore(deps) : remeda, radash,  typia, vitest 추가

--- downloader/vite.config.ts
+++ downloader/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import UnpluginTypia from "@ryoppippi/unplugin-typia/vite";
+import solid from "vite-plugin-solid";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solid(), UnpluginTypia()],
+});
