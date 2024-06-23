74a2fd6f2f2e1347022e9af67017fb59d97f6d85
devcomfort <kimdonghyun026@gmail.com>
2024-06-23 09:11:17 +0000

    feat : 파일 확장자 교정 기능 추가

--- downloader/src/libs/url-handler/url-handler.ts
+++ downloader/src/libs/url-handler/url-handler.ts
@@ -1,6 +1,6 @@
 import { pipe, split } from "remeda";
 import { trim } from "lodash/fp";
-import path from "path";
+import path from "path-browserify";
 
 import { DownloadRequest } from "../download-handler/schema";
 
@@ -17,7 +17,34 @@ class URLHandler {
       );
 
     let [url, filename] = splitted;
-    filename ??= URLHandler.extractFileName(url);
+
+    // NOTE: 파일 이름이 누락된 경우, URL에서 파일 이름을 추출하여 사용
+    filename ??= URLHandler.basename(url);
+
+    // NOTE: 파일 이름의 파일 확장자 교정
+
+    /**
+     * 파일 확장자
+     *
+     * 저장할 파일의 확장자 교정을 위해 다음과 같은 우선순위로 파일 확장자를 가져옴
+     *
+     * 1. URL에 포함된 확장자 (예시: `"https://www.google.com/video.mp4"` -> `".mp4"`)
+     * 2. 파일 이름에 포함된 확장자 (예시: `"video.mp4"` -> `".mp4"`)
+     * 3. 빈 문자열 ("")
+     *
+     * path.extanme(path)에서 path에 확장자가 없는 경우, 빈 문자열 `""`를 반환함
+     */
+    const ext =
+      path.extname(url).length > 0 ? path.extname(url) : path.extname(filename);
+
+    filename = path.join(
+      path.join(
+        // 파일 이름 (확장자 제외, 예시: `"video.mp4"` -> `"video"`)
+        path.basename(filename, path.extname(filename)),
+        // 파일 확장자 (ext 변수 참조)
+        ext
+      )
+    );
 
     return {
       filename,
@@ -26,15 +53,16 @@ class URLHandler {
   }
 
   /** URL에서 파일 이름을 추출합니다 */
-  static extractFileName(url: string) {
-    return path.parse(url).base;
+  static basename(url: string) {
+    return path.basename(url);
   }
 
   /**
    * `<URL>;<filename>` 구조로 URL과 filename을 병합하여 반환합니다
    */
   static stringify({ url, filename }: DownloadRequest): string {
-    filename ??= URLHandler.extractFileName(url);
+    filename ??= URLHandler.basename(url);
+
     return `${url};${filename}`;
   }
 }
