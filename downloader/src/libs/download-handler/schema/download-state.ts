93bf2930450d3f3025a2b9eb478ad9200bb53756
devcomfort <kimdonghyun026@gmail.com>
2024-06-24 04:16:58 +0000

    fix : 다운로드 상태를 표시하는데 불필요한 내용 제거

--- downloader/src/libs/download-handler/schema/download-state.ts
+++ downloader/src/libs/download-handler/schema/download-state.ts
@@ -1,16 +1,5 @@
-import { tags } from "typia";
-
-/** URL 타입 */
-export type Url = string & tags.Format<"url">;
-
 /** 다운로드 상태 인터페이스 */
 export interface DownloadState {
-  /** 파일 이름 (파일 저장 시 사용)
-   * @description 입력된 값이 없는 경우 `null`로 처리, `url`에서 추출하여 사용
-   */
-  filename: string | null;
-  /** 다운로드 대상 URL */
-  url: Url;
   /**
    * 다운로드 진행 상태
    *
